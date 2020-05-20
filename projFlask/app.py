117c98640dea0012c0400bb247f40397037bde40
Thomas <syvarris.toto@gmail.com>
2020-05-20 14:25:14 +0200

    correct

---
+++ projFlask/app.py
@@ -0,0 +1,19 @@
+
+from flask import Flask
+import pandas as pd
+app = Flask(__name__)
+
+data = pd.read_json('books.json')
+print(data.head())
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World From a Docker container'
+
+@app.route('/books')
+def getBooks():
+    return data.head().to_json(orient='records')
+
+@app.route('/book/<isbn>')
+def getBookByISBN(isbn):
+    return data[data['isbn'] == isbn].to_dict()
---
+++ projFlask/testFlask.sh
@@ -0,0 +1,3 @@
+curl 127.0.0.1:5000/books \
+&& echo 'Recherche du livre Android in Action, Second Edition' \
+&& curl 127.0.0.1:5000/book/1935182722
